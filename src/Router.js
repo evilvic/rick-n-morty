c9c6dcbf0a1acf9385d9f2e3c707bba891fb7135
Víctor Peña Romero <vic.pero@icloud.com>
2020-04-17 19:40:34 -0500

    add router

---
+++ src/Router.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Characters from './pages/Characters'
+import Episodes from './pages/Episodes'
+import Register from './pages/Register'
+
+const Router = () => (
+    <>
+        <Navbar />
+        <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/characters' component={Characters} />
+            <Route exact path='/episodes' component={Episodes} />
+            <Route exact path='/register' component={Register} />
+        </Switch>
+    </>
+)
+
+export default Router
