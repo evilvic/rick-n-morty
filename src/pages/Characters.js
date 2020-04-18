d16b1ff6f8f327b93bf220ad9499c04ae2bff69f
Víctor Peña Romero <vic.pero@icloud.com>
2020-04-17 19:14:57 -0500

    add pages

---
+++ src/pages/Characters.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Characters = () => {
+    return (
+        <h2>Characters</h2>
+    )
+}
+
+export default Characters
---
+++ src/pages/Episodes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Episodes = () => {
+    return (
+        <h2>Episodes</h2>
+    )
+}
+
+export default Episodes
