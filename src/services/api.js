e68d129210fb753c3bce2c3823b3c336b74a3bc8
Víctor Peña Romero <vic.pero@icloud.com>
2020-04-17 19:05:59 -0500

    add api service

---
+++ src/services/api.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+const baseURL = 'https://rickandmortyapi.com/api'
+
+const MY_SERVICE = axios.create({
+    baseURL,
+    withCredentials: true
+})
+
+const API_SERVICE = {
+    getCharacters: async () => {
+        return await MY_SERVICE.get('/character')
+    },
+    getEpisodes: async () => {
+        return await MY_SERVICE.get('/episode')
+    }
+}
+
+export default API_SERVICE
